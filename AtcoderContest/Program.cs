4dadf3eeac3c52580e853b6b62980b5ec6bd1696
younghiro <younghiromu112010@icloud.com>
2022-10-23 13:40:00 +0200

    how to solve decimal

--- AtcoderContest/Program.cs
+++ AtcoderContest/Program.cs
@@ -7,54 +7,52 @@ namespace AtcoderContest
         static void Main(string[] args)
         {
             //int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-            //int[] output = new int[6];
-            //int nana = input[0]; //7
-            //int yon= input[1];//4
-            //for ( int s = 0; s<6; s++)
+            //char[,] X = new char[input[0], input[1]];//今回は2x2の行列で考える
+            //int[] outputs = new int[input[1]];
+            //for (int i = 0; i < input[0]; i++)
             //{
-            //    int sho = yon / nana;
-            //    output[s] = sho;
-            //    sho = yon * 10/nana;
-            //}
-            //if (output[6] >= 5)
-            //{
-            //    output[5]++;
+            //    char[] gyou = Console.ReadLine().ToCharArray();
+            //    for (int j = 0; j < input[1]; j++)
+            //    {
+            //        X[i, j] = gyou[j];//列への割り当て
+            //    }
             //}
 
-            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-            char[,] X = new char[input[0], input[1]];//今回は2x2の行列で考える
-            int[] outputs = new int[input[1]];
-            for (int i = 0; i < input[0]; i++)
-            {
-                char[] gyou = Console.ReadLine().ToCharArray();
-                for (int j = 0; j < input[1]; j++)
-                {
-                    X[i, j] = gyou[j];//列への割り当て
-                }
-            }
+            //for (int j = 0; j < input[1]; j++)
+            //{
 
-            for (int j = 0; j < input[1]; j++)
-            {
-                
-                int output = 0;
-                for (int i = 0; i < input[0]; i++)
-                {
-                    string a = X[i, j].ToString();
-                    if( a == "#")
-                    {
-                        output++;
-                    }
-                }
+            //    int output = 0;
+            //    for (int i = 0; i < input[0]; i++)
+            //    {
+            //        string a = X[i, j].ToString();
+            //        if( a == "#")
+            //        {
+            //            output++;
+            //        }
+            //    }
 
-                outputs[j] = output;
-            }
+            //    outputs[j] = output;
+            //}
 
-            foreach( int r in outputs)
-            {
-                Console.Write(r);
-                Console.Write(" ");
-            }
-        }
+            //foreach( int r in outputs)
+            //{
+            //    Console.Write(r);
+            //    Console.Write(" ");
+            //}
+
+           
+                string str = Console.ReadLine();
+                string[] strs = str.Split();
+                double douA = double.Parse(strs[0]);
+                double douB = double.Parse(strs[1]);
+
+                //double douA = double.Parse(str.Substring(0,1));
+                //double douB = double.Parse(str.Substring(2,1));
+
+                double ans = Math.Round(douB / douA, 3, MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{ans:F3}");
+            
+}
     }
 }
 
